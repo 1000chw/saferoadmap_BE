d30f64fb732b2b2ae801b26521a42a9496826370
Chun Hyunwoo <1000chw@gmail.com>
2023-08-16 00:41:44 +0900

    feat: POST /photo/report
    사진 id를 받아 신고하는 API 작성
    analysis를 위해 사진 받고 후에 S3에 저장하는 함수 작성

---
+++ config/database.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: `${process.env.DB_HOST}`,
+  user: `${process.env.DB_USER}`,
+  port: `${process.env.DB_PORT}`,
+  password: `${process.env.DB_PASSWORD}`,
+  database: `${process.env.DB_DATABASE}`,
+});
+
+export default pool;
--- config/express.js
+++ config/express.js
@@ -4,6 +4,7 @@ import yaml from 'js-yaml';
 import fs from 'fs';
 import cors from 'cors';
 import pathfindingRouter from '../src/pathfinding/pathfindingRouter';
+import photoRouter from '../src/photo/photoRouter';
 
 const app = express();
 const swaggerSpec = yaml.load(fs.readFileSync('./swagger/swagger.yaml',  'utf8'));
@@ -13,7 +14,9 @@ app.get('/', (req, res) => res.status(200).send('app start'));
 app.use(cors());
 app.use(express.json());
 
-app.use('/pathfinding', pathfindingRouter)
+app.use('/pathfinding', pathfindingRouter);
+
+app.use('/photo', photoRouter);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
