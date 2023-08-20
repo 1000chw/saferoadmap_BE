8a8c705aae9d3232c32eb29ae8d190cab1d703a3
Chun Hyunwoo <1000chw@gmail.com>
2023-08-20 13:58:53 +0000

    Revert "Merge branch 'feature/8-busArrival' into 'production'"
    This reverts merge request !15

--- config/express.js
+++ config/express.js
@@ -3,22 +3,13 @@ import swaggerUi from 'swagger-ui-express';
 import yaml from 'js-yaml';
 import fs from 'fs';
 import cors from 'cors';
-import pathfindingRouter from '../src/pathfinding/pathfindingRouter';
-import photoRouter from '../src/photo/photoRouter';
-import arrivalRouter from '../src/arrival/arrivalRouter';
 
 const app = express();
-const swaggerSpec = yaml.load(fs.readFileSync('./swagger/swagger.yaml',  'utf8'));
+const swaggerSpec = yaml.load(fs.readFileSync('./swagger/openapi.yaml',  'utf8'));
 
 app.get('/health', (req, res) => res.status(200).send('OK'));
 app.get('/', (req, res) => res.status(200).send('app start'));
 app.use(cors());
-app.use(express.json());
-
-app.use('/pathfinding', pathfindingRouter);
-app.use('/photo', photoRouter);
-app.use('/arrival', arrivalRouter);
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
--- src/pathfinding/pathfindingRouter.js
+++
@@ -1,9 +0,0 @@
-import pathfindingController from './pathfindingController';
-import express from 'express';
-
-const pathfindingRouter = express.Router();
-
-pathfindingRouter.get('/pedestrain', pathfindingController.getPedestrainPath);
-pathfindingRouter.get('/transport', pathfindingController.getTransportPath);
-
-export default pathfindingRouter;
