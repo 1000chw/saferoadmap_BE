a43ef4668a2547428ba35e0da1b2d0b64a810a04
Baejjjjjjjj <rew1212@khu.ac.kr>
2023-08-24 04:50:39 +0900

    feature: 신호등 데이터 변경 api response로 요청을 보내 횡단보도 좌표 값을 알아내는 부분까지 진행 fixes #9

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+import { scheduleJob } from 'node-schedule';
 import app from './config/express';
 
-app.listen(3000, () => {console.log('Server is running on port 3000')});
+app.listen(3000, () => {
+    /*scheduleJob('* * 1 * * *', function() {
+
+    });*/
+    console.log('Server is running on port 3000')});
