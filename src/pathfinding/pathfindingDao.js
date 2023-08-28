adb89af6555c90eefe2005fcd5e18220c57c246f
Chun Hyunwoo <1000chw@gmail.com>
2023-08-29 04:34:10 +0900

    feat: 로직 첫 번째 방법
     앞에서부터 하나씩 signal_generator = true로 만드는 로직 구현

--- src/pathfinding/pathfindingDao.js
+++ src/pathfinding/pathfindingDao.js
@@ -2,12 +2,22 @@ const pathfindingDao = {
     checkSignalGenerator: async (connection, x, y) => {
         const sql = `select ST_X(pp), ST_Y(pp), ST_Distance_Sphere(POINT(${x}, ${y}), pp) as distance from info where ST_Distance_Sphere(POINT(${x}, ${y}), pp) <= 16 order by distance asc LIMIT 1`;
         try {
-            const [result] = await connection.query(sql);
+            const [[result]] = await connection.query(sql);
             if (!result) return {result: -1};
             return result;
         } catch (error) {
             console.log(error);
             return {error: "음향 신호기 여부 확인 중 문제가 발생했습니다."}   
+        }
+    },
+    selectNearSignalGenerator: async (connection, x, y) => {
+        const sql = `select ST_X(pp) as X, ST_Y(pp) as Y from info order by ST_Distance_Sphere(POINT(${x}, ${y}), pp) asc LIMIT 10`;
+        try {
+            const [result] = await connection.query(sql);
+            return result;
+        } catch (error) {
+            console.log(error);
+            return {error: "근처 음향 신호기 위치 확인 중 문제가 발생했습니다."}   
         }
     },
 
--- src/pathfinding/pathfindingProvider.js
+++ src/pathfinding/pathfindingProvider.js
@@ -4,48 +4,125 @@ import pool from "../../config/database";
 import pathfindingDao from './pathfindingDao';
 
 const headers = {
-    "appKey": process.env.TMAP_APP_KEY2
+    "appKey": process.env.TMAP_APP_KEY
 };
 
+const findPath = async (startX, startY, endX, endY, startName, endName, passList) => {
+    if (!passList || !passList.length) {
+        const path = await axios.post('https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1',{
+            "startX": startX,
+            "startY": startY,
+            "endX": endX,
+            "endY": endY,
+            "startName": encodeURIComponent(startName),
+            "endName": encodeURIComponent(endName)
+        }, { headers }).catch((err) => {return {error: err.response}});
+        return path.data;
+    }
+    else{
+        const path = await axios.post('https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1',{
+            "startX": startX,
+            "startY": startY,
+            "endX": endX,
+            "endY": endY,
+            "startName": encodeURIComponent(startName),
+            "endName": encodeURIComponent(endName),
+            "passList": passList.join("_")
+        }, { headers }).catch((err) => {return {error:err.response}});
+        return path.data;
+    }
+}
+
 const pathfindingProvider = {
-    getPedestrainPath: async (startX, startY, endX, endY, startName, endName, passList, x, y) =>{
+    getPedestrainPath: async (startX, startY, endX, endY, startName, endName) =>{
         try{
+            let chk = false;
             let result = {};
-            await axios.post('https://apis.openapi.sk.com/tmap/routes/pedestrian?version=1',{
-                "startX": startX,
-                "startY": startY,
-                "endX": endX,
-                "endY": endY,
-                "startName": encodeURIComponent(startName),
-                "endName": encodeURIComponent(endName),
-                "passList": passList
-            }, { headers }).then(response => {
-                result = response.data;
-            }).catch(err => {
-                result = {error: err.response.data};
-            });
-            if (!result.error){
-                for (const i in result.features){
-                    const point = result.features[i];
-                    if (point.properties.facilityType && point.properties.facilityType === "15" && 211 <= point.properties.turnType && point.properties.turnType <= 217){
-                        const [x, y] = point.geometry.coordinates;
-                        const connection = await pool.getConnection();
-                        const check = await pathfindingDao.checkSignalGenerator(connection, x, y);
-                        connection.release();
-                        console.log(check); 
-                        if (check.error){
-                            result = check;
-                            break;
+            let passList = [[]];
+            let passListLog = [];
+            let passListIndex = 0;
+            let crossList = [];
+            let firstPath = [];
+            let lastPath = [];
+            let firstCheck = false;
+            while (!chk){
+                result = await findPath(startX, startY, endX, endY, startName, endName, passList[passListIndex]);
+                chk = true;
+                if (!result.error){
+                    if (!firstCheck) {
+                        firstPath = result.features;
+                        firstCheck = true;
+                        lastPath.push(result.features[0]);
+                    }
+                    for (const i in result.features){
+                        if (!i) continue;
+                        if (!chk) break;
+                        const point = result.features[i];
+                        if (point.properties.pointType && point.properties.pointType.slice(0,2) === "PP"){
+                            result.features[i].signal_generator = true;
+                            continue;
                         }
-                        if (check.result === -1){
-                            result.features[i].signal_generator = false;
+                        if (point.properties.facilityType && point.properties.facilityType === "15" && 211 <= point.properties.turnType && point.properties.turnType <= 217){
+                            let [x, y] = point.geometry.coordinates;
+                            const connection = await pool.getConnection();
+                            const check = await pathfindingDao.checkSignalGenerator(connection, x, y);
+                            if (check.error){
+                                result = check;
+                                break;
+                            }
+                            if (check.result === -1){
+                                result.features[i].signal_generator = false;
+                                if (passList[passListIndex].length === 5) {
+                                    lastPath = lastPath.concat(result.features.slice(1, i))
+                                    [startX, startY] = result.features[i-2].geometry.coordinates;
+                                    startName = result.features[i-2].properties.name;
+                                    chk = false;
+                                    passList.push([]);
+                                    crossList = [];
+                                    passListIndex += 1;
+                                    break;
+                                }
+                                if (i >= 2) [x, y] = result.features[i-2].geometry.coordinates;
+                                const nearSignalGenerator = await pathfindingDao.selectNearSignalGenerator(connection, x, y);
+                                
+                                for(const j in nearSignalGenerator){
+                                    const signalGenerator = nearSignalGenerator[j];
+                                    const sg = passList[passListIndex].find(element => element === `${signalGenerator.X},${signalGenerator.Y}`);
+                                    const c = crossList.find(element => element === `${x},${y}`);
+                                    const sgLog = passListLog.find(element => element === `${signalGenerator.X},${signalGenerator.Y}`);
+                                    if (!sg && !c) {
+                                        passList[passListIndex].push(`${signalGenerator.X},${signalGenerator.Y}`);
+                                        passListLog.push(`${signalGenerator.X},${signalGenerator.Y}`);
+                                        crossList.push(`${x},${y}`);
+                                        chk = false;
+                                        break;
+                                    }
+                                    if (sgLog && c){
+                                        break;
+                                    }
+                                    if (sg && c) {
+                                        passList[passListIndex].pop(sg);
+                                        crossList.pop(c);
+                                    }
+                                }
+                            }
+                            else result.features[i].signal_generator = true;
+                            
+                            connection.release();
                         }
-                        else result.features[i].signal_generator = true;
+                        if (Number(i) == result.features.length - 1){
+                            lastPath = lastPath.concat(result.features.slice(1, result.features.length));
+                        }
                     }
                 }
             }
-
-            return result;
+            lastPath[0].properties.totalDistance = 0;
+            lastPath[0].properties.totalTime = 0;
+            for (const i in lastPath){
+                if (lastPath[i].properties.time) lastPath[0].properties.totalTime += lastPath[i].properties.time;
+                if (lastPath[i].properties.distance) lastPath[0].properties.totalDistance += lastPath[i].properties.distance;
+            }
+            return {firstPath: firstPath, lastPath: lastPath};
         }catch(err){
             console.log(err);
             return {error: true};
