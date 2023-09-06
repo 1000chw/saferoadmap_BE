85d2ce4c2c7178cbd4833a294cebfae86e2e8f5a
Baejjjjjjjj <rew1212@khu.ac.kr>
2023-09-06 19:32:06 +0900

    fix: ai 기능 수정

--- src/photo/photoController.js
+++ src/photo/photoController.js
@@ -2,12 +2,84 @@ import photoService from "./photoService";
 import photoProvider from "./photoProvider";
 import photo from "../../config/s3.js";
 import fs from 'fs';
-
+import automl from "@google-cloud/automl";
+import  aiplatform from "@google-cloud/aiplatform";
+import { param } from "express-validator";
+import {PredictionServiceClient} from '@google-cloud/aiplatform';
+    
+  
 const photoController = {
     photoAnalysis: async (req, res) => {
-        const image = fs.readFileSync(req.file.path);
-        const result = await photo.s3Upload(image, req.file.originalname, req.file.mimetype);
+        try{
+        const images = fs.readFileSync(req.file.path, 'base64');
+ 
+        const result = 0;//await photo.s3Upload(images, req.file.originalname, req.file.mimetype);
+        const filename = "KakaoTalk_20230217_230928064.png";
+        const endpointId = "4412755777660387328";
+        const project = 'ordinal-door-395311';
+        const location = 'us-central1';
+
+        const {instance, params, prediction} =
+  aiplatform.protos.google.cloud.aiplatform.v1.schema.predict;
+
+// Imports the Google Cloud Prediction Service Client library
+const {PredictionServiceClient} = aiplatform.v1;
+
+// Specifies the location of the api endpoint
+const clientOptions = {
+  apiEndpoint: 'us-central1-aiplatform.googleapis.com',
+};
+
+// Instantiates a client
+const predictionServiceClient = new PredictionServiceClient(clientOptions);
+
+async function predictImageClassification() {
+  // Configure the endpoint resource
+  const endpoint = `projects/${project}/locations/${location}/endpoints/${endpointId}`;
+
+  const parametersObj = new params.ImageClassificationPredictionParams({
+    confidenceThreshold: 0.5,
+    maxPredictions: 5,
+  });
+  const parameters = parametersObj.toValue();
+
+  const fs = require('fs');
+  const image = fs.readFileSync(filename, 'base64');
+  const instanceObj = new instance.ImageClassificationPredictionInstance({
+    content: image,
+  });
+  const instanceValue = instanceObj.toValue();
+
+  const instances = [instanceValue];
+  const request = {
+    endpoint,
+    instances,
+    parameters,
+  };
+
+  // Predict request
+  const [response] = await predictionServiceClient.predict(request);
+
+  console.log('Predict image classification response');
+  console.log(`\tDeployed model id : ${response.deployedModelId}`);
+  const predictions = response.predictions;
+  console.log('\tPredictions :');
+  for (const predictionValue of predictions) {
+    const predictionResultObj =
+      prediction.ClassificationPredictionResult.fromValue(predictionValue);
+    for (const [i, label] of predictionResultObj.displayNames.entries()) {
+      console.log(`\tDisplay name: ${label}`);
+      console.log(`\tConfidences: ${predictionResultObj.confidences[i]}`);
+      console.log(`\tIDs: ${predictionResultObj.ids[i]}\n\n`);
+    }
+  }
+}
+predictImageClassification();
+        
         return res.status(200).json({code: 1005, message: "사진 업로드 성공", result: result});
+        }catch(err){
+            console.log(err)
+        }
     },
 
     postReport: async (req, res) => {
